62fca86855e45b95c5921e6d006bea9db3839711
Patrick Chung <cyber@Patricks-MacBook-Air.local>
2021-10-06 20:54:18 -0700

    .

--- 3 ooo/addtl.py
+++ 3 ooo/addtl.py
@@ -26,3 +26,25 @@ import random
 import threading
 import time
 from functools import reduce
+
+# DEBUG TITLE
+print("\n\naddtl.py\n.\n.\n.\n.\n\n")
+
+str1 = "Escape Sequences \' \" \t \\ \n"
+
+# So these things were always called escape sequences, the things that allows
+#   things like single and double quotes and newlines and tabs to be used
+#   inside strings.
+# List of escape sequences
+#   \'      makes the apostrphe
+#   \"      makes the quotation
+#   \\      makes the backslashes
+#   \t      makes the tabs
+#   \n      makes newline
+
+#Practice zone
+
+print("EXAMPLE\n")
+
+print("does print() automatically create a newline?")
+print("lets find out")
