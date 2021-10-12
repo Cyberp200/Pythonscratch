55edaf5c04342447d09f6e2d496b731c0789ed3b
Patrick Chung <cyber@Patricks-MacBook-Air.local>
2021-10-11 20:18:51 -0700

    .

--- 5 cond/cond.py
+++ 5 cond/cond.py
@@ -29,3 +29,27 @@ from functools import reduce
 
 # DEBUG TITLE
 print("\n\ncond.py\n.\n.\n.\n.\n\n")
+
+x = int(input("Type some fucking number"))
+
+if x > 21:
+    print("x is greater than 21")
+elif x >= 16:
+    print("x is greater or equal to 16")
+else:
+    print("fuck....")
+
+'''
+You will notice that in python3
+
+if x > 21:
+(TAB) print("x is greater than 21")
+elif x >= 16:
+(TAB) print("x is greater or equal to 16")
+else:
+(TAB) print("fuck....")
+
+
+Python3 must use tabs and nextlines in this exact
+configuration for conditionals to function properly.
+'''
