c368f5670aae35705db83485fc3705cfa47599bf
Patrick Chung <cyber@Patricks-MacBook-Air.local>
2021-10-17 16:11:03 -0700

    .

--- 3 ooo/addtl.py
+++ 3 ooo/addtl.py
@@ -77,3 +77,45 @@ print(format_string % data, end = '\n\n')
 # %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
 
 # %x/%X - Integers in hex representation (lowercase/uppercase)
+
+
+'''
+MORE ON Strings
+'''
+
+# RAW STRINGS
+
+print(r"I'll be ignoring escape sequences in this string like this \t\n")
+
+# String concatenates
+
+print("\nHello " + "World")
+
+# SPECIAL STRING FUNCTIONS TO PARSE STRINGS WITH
+
+# String length
+
+stringEx = "Hello fucker"
+
+print("Using len() to get length of string ->", len(stringEx), "The string was", stringEx)
+
+# Return specific character in string
+
+#   get the first character
+print('getting the first character, use stringEx[0] ->', stringEx[0])
+
+#   get the last character (its pretty simple)
+print('getting the last character, use stringEx[-1] ->', stringEx[-1])
+
+#   getting just the 'e' in the word "fucker"
+print('getting the \'e\' in \"fucker\", use stringEx[-2] ->', stringEx[-2])
+#       doing it again using the other direction of the char array
+print('getting the \'e\' in \"fucker\", but in the opposite direction, use stringEx[10] ->', stringEx[10])
+
+#   get a range of characters displayed
+print('getting the \'Hello\', or the first 5 characters, use stringEx[0:5] ->', stringEx[0:5])
+print("Up to but not include the 5th index")
+
+#   get a range of characters displayed
+print('getting only every other character, use stringEx[0:-1:2] ->', stringEx[0:-1:2])
+print("the 0 and the -1 is the range, and the 2 is the increment in which to skip up to")
