60f08320eacda3ad2deeab19214e7610f2be9873
Patrick Chung <cyber@Patricks-MacBook-Air.local>
2021-11-23 19:08:19 -0800

    Added new practice line from w3resource

---
+++ w3resource/one.py
@@ -0,0 +1,41 @@
+# one.py
+
+'''
+Write a Python program to print the following string in a specific format (see the output). Go to the editor
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :
+
+Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are
+'''
+
+# 20211123
+
+# Patrick Chung
+# cyberp200@rocketmail.com
+# 562-480-2114
+
+'''
+              ___.                       _______________  _______
+  ____ ___.__.\_ |__   _________________ \_____  \   _  \ \   _  \
+_/ ___<   |  | | __ \_/ __ \_  __ \____ \ /  ____/  /_\  \/  /_\  \
+\  \___\___  | | \_\ \  ___/|  | \/  |_> >       \  \_/   \  \_/   \
+ \___  > ____| |___  /\___  >__|  |   __/\_______ \_____  /\_____  /
+     \/\/          \/     \/      |__|           \/     \/       \/
+
+'''
+
+# Imports
+
+import sys
+import math
+import random
+import threading
+import time
+from functools import reduce
+
+# DEBUG TITLE
+print("\n\none.py\nw3resource\n.\n.\n.\n.\n\n")
